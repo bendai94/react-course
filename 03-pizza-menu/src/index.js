f8bfbb78ea6aa423ce173396f3c41cf071e4a4d5
bendai94 <bendai94@gmail.com>
2023-10-10 17:25:33 -0400

    Fix comment

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -10,7 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>);
+    </React.StrictMode>
+);
 
 // React before 18
-// React.render(<App />);
+// React.render(<App />, document.getElementById("root"));
